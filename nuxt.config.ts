061c9e2b94a72713a4fe7210ed462effce86086e
twoflyingforks <93388486+twoflyingforks@users.noreply.github.com>
2025-06-28 00:48:43 +0800

    shadcn with sidebar07

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  modules: ["@nuxt/eslint", "shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
